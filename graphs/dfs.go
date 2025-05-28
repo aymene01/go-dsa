9de990a0ef9b3aecb29a4142e30a4117b09ed15b
Aymene BOUSBIA <aymene.bousbia@ledger.fr>
2025-05-28 14:02:29 +0200

    feat: recusive dfs for graphs

--- graphs/dfs.go
+++ graphs/dfs.go
@@ -1,1 +1,24 @@
 package graphs
+
+func dfs(graph map[string][]string, src, dst string, visited map[string]bool) bool {
+	if src == dst {
+		return true
+	}
+
+	if seen, ok := visited[src]; ok && seen {
+		return false
+	}
+
+	visited[src] = true
+	for _, neighbor := range graph[src] {
+		if dfs(graph, neighbor, dst, visited) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func HasPath(graph map[string][]string, src, dst string) bool {
+	return dfs(graph, src, dst, make(map[string]bool))
+}
